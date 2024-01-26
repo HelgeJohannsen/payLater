f023b424c15dc609612cb5d55090b00b50a131a1
valdenicio22 <valdenicio22@gmail.com>
2024-01-26 11:09:27 +0100

    feat: remove handle from validation

--- extensions/paylater-checkout-ext/src/Checkout.tsx
+++ extensions/paylater-checkout-ext/src/Checkout.tsx
@@ -17,41 +17,16 @@ function Extension() {
   const selectedPaymentMethod = useSelectedPaymentOptions();
   const { countryCode } = useShippingAddress();
   const checkoutTotalValue = cost.totalAmount.current.amount;
-  const currentPaymentHandle = selectedPaymentMethod[0].handle;
   let errorMsg = "";
-
-  //console.log("selectedPaymentMethod", selectedPaymentMethod)
-
-  const paymentMethodsMap = {
-    "Kauf auf Rechnung by Consors Finanz": {
-      handle: "custom-manual-payment-62fc1293cd0487b5051a0b563353e222",
-      minimumValue: 9.99,
-    },
-    "Kauf per Lastschrift by Consors Finanz": {
-      handle: "custom-manual-payment-966e6977353fea0d0827e9331dd5f139",
-      minimumValue: 9.99,
-    },
-    "3-Monats-Zahling by Consors Finanz": {
-      handle: "custom-manual-payment-890d1d258371367a5139991406a10a6e",
-      minimumValue: 26.99,
-    },
-  };
 
   const isProcessBlock = () => {
     if (selectedPaymentMethod[0].type !== "manualPayment") return false;
 
-    return Object.entries(paymentMethodsMap).some((pm) => {
-      const key = pm[0];
-      const { handle, minimumValue } = pm[1];
-      if (handle !== currentPaymentHandle) return false;
-      if (minimumValue < checkoutTotalValue) return false;
-      else {
-        errorMsg = `Apologies, but a minimum value of ${minimumValue} Euro is required for ${key}
-        payment method.`;
-
-        return true;
-      }
-    });
+    if (9.99 < checkoutTotalValue) return false;
+    else {
+      errorMsg = `Apologies, but a minimum value of 9.99 Euro is required for Consors Finanz payment method.`;
+      return true;
+    }
   };
 
   useBuyerJourneyIntercept(({ canBlockProgress }) => {
