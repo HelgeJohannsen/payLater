d1a14cc0918b1e31ac3a1b1fb720639c2339f372
valdenicio22 <valdenicio22@gmail.com>
2024-01-26 10:05:04 +0100

    refactor: TS improviments

--- extensions/paylater-checkout-ext/src/utils/index.ts
+++ extensions/paylater-checkout-ext/src/utils/index.ts
@@ -1,17 +1,18 @@
+import type { MailingAddress } from "@shopify/ui-extensions/checkout";
 import type { Address } from "../types";
 
-function isAddressEqual(obj1, obj2) {
-  const getAddress = (obj) => {
+function isAddressEqual(obj1: MailingAddress, obj2: MailingAddress) {
+  const getAddress = (obj: MailingAddress): Address => {
     return {
       address1: obj.address1,
-      address2: obj.address2,
+      address2: obj?.address2,
       countryCode: obj.countryCode,
       city: obj.city,
       zip: obj.zip,
     };
   };
-  const shippingAddress: Address = getAddress(obj1);
-  const billingAddress: Address = getAddress(obj2);
+  const shippingAddress = getAddress(obj1);
+  const billingAddress = getAddress(obj2);
 
   if (
     Object.keys(shippingAddress).length !== Object.keys(billingAddress).length
