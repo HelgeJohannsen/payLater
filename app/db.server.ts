1370646020dcce8de477ef6329b27f330374e0df
Helge <helgemmo@googlemail.com>
2024-01-27 23:30:18 +0100

    webhook fix

--- app/db.server.ts
+++ app/db.server.ts
@@ -5,7 +5,7 @@ declare global {
 }
 
 const prisma: PrismaClient = global.prisma || new PrismaClient();
-
+console.log("production env proc",process.env.NODE_EN)
 if (process.env.NODE_ENV !== "production") {
   console.log("production env")
   if (!global.prisma) {
