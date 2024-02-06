c376151d7719995c29cb4be9436cdb70943cf55b
valdenicio22 <valdenicio22@gmail.com>
2024-02-06 16:56:34 +0100

    feat: remove minimum cost validation

--- extensions/paylater-checkout-ext/src/Checkout.tsx
+++ extensions/paylater-checkout-ext/src/Checkout.tsx
@@ -3,7 +3,6 @@ import {
   useApi,
   useBuyerJourneyIntercept,
   useSelectedPaymentOptions,
-  useShippingAddress,
 } from "@shopify/ui-extensions-react/checkout";
 import { isAddressEqual } from "./utils";
 
@@ -13,19 +12,13 @@ export default reactExtension(
 );
 
 function Extension() {
-  const { cost, billingAddress, shippingAddress } = useApi();
+  const { billingAddress, shippingAddress } = useApi();
   const selectedPaymentMethod = useSelectedPaymentOptions();
-  const { countryCode } = shippingAddress?.current
-  const checkoutTotalValue = cost.totalAmount.current.amount;
+  const { countryCode } = shippingAddress?.current;
   let errorMsg = "";
-
-  console.log("selectedPaymentMethod", selectedPaymentMethod);
-  console.log("shippingAddress, billingAddress", shippingAddress.current, billingAddress.current);
 
   const isProcessAllow = () => {
     if (selectedPaymentMethod[0].type !== "manualPayment") return true;
-
-    if (9.99 < checkoutTotalValue) return true;
     else {
       errorMsg = `Apologies, but a minimum value of 9.99 Euro is required for Consors Finanz payment method.`;
       return false;
@@ -33,28 +26,25 @@ function Extension() {
   };
 
   useBuyerJourneyIntercept(({ canBlockProgress }) => {
-    const isPayLaterPossible =
-      countryCode === "DE" &&
-      !isProcessAllow() &&
-      isAddressEqual(shippingAddress.current, billingAddress.current);
-
-    return countryCode === "DE" && isProcessAllow() ? {
-      behavior: "allow",
-      }
-    : isAddressEqual(shippingAddress.current, billingAddress.current) ?
-    {
-      behavior: "block",
-      reason: "Minimum value",
-      errors: [
-        {
-          message: errorMsg
-            ? errorMsg
-            : `Apologies, but the shipping address and billing address need to match when using a Consors Finanz payment method.`,
-        },
-      ],
-    }: {
-      behavior: "allow",
-      }
+    return canBlockProgress && countryCode === "DE" && isProcessAllow()
+      ? {
+          behavior: "allow",
+        }
+      : isAddressEqual(shippingAddress.current, billingAddress.current)
+      ? {
+          behavior: "block",
+          reason: "Minimum value",
+          errors: [
+            {
+              message: errorMsg
+                ? errorMsg
+                : `Apologies, but the shipping address and billing address need to match when using a Consors Finanz payment method.`,
+            },
+          ],
+        }
+      : {
+          behavior: "allow",
+        };
   });
 
   return <></>;
