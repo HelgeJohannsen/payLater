daa035bd40ee71a133317f2637353c05bdf70bc1
valdenicio22 <valdenicio22@gmail.com>
2024-02-12 15:33:12 +0100

    refactor: prisma generate

--- app/models/config.server.ts
+++ app/models/config.server.ts
@@ -11,17 +11,17 @@ export async function getOrCreateConfig(shop: string) {
   return config;
 }
 export async function createConfig(shop: string) {
-    /** @type {any} */
-    const data = {
-      customerAccountNumber: "Test123456789",
-      vendorId: "8403",
-      shop: shop
-    };
-  
-    const Settings = await db.config.create({ data });
-  
-    if (!Settings) {
-      return null;
-    }
-    return Settings;
+  /** @type {any} */
+  const data = {
+    customerAccountNumber: "Test123456789",
+    vendorId: "8403",
+    shop: shop,
+  };
+
+  const Settings = await db.config.create({ data });
+
+  if (!Settings) {
+    return null;
   }
+  return Settings;
+}
