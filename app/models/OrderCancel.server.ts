3801ce9761a1237fdf27ea15199196dcf7e78a1c
valdenicio22 <valdenicio22@gmail.com>
2024-02-20 00:49:30 +0000

    feat: add cancel notify route

---
+++ app/models/OrderCancel.server.ts
@@ -0,0 +1,26 @@
+import db from "../db.server";
+
+export async function handleOrderCancel(orderId: string, status: string) {
+  try {
+    const updatedOrder = await db.orders.update({
+      where: { orderId },
+      data: { cancelStatus: status },
+    });
+    return updatedOrder;
+  } catch (error) {
+    console.error("Order update failed", error);
+  }
+}
+export async function getApplicationReferenceNumber(orderId: string){
+  try{
+    const applicationNum = await db.orders.findUnique({
+      where: {orderId},
+      select: {
+        applicationNumber: true
+      }
+    })
+    return applicationNum
+  }catch(error){
+    console.error("Unable to get Application Reference Number")
+  }
+}
---
+++ app/routes/notify.cancelOrder.ts
@@ -0,0 +1,24 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { handleOrderCancel } from "~/models/OrderCancel.server";
+
+export const loader: LoaderFunction = async ({ request, params }) => {
+  const requestedURL = new URL(request.url);
+  const orderId = requestedURL.searchParams.get("orderId");
+  const status = requestedURL.searchParams.get("status");
+
+  // const hash = requestedURL.searchParams.get("hash");
+  if (orderId === null || status === null ) {
+    throw new Response(
+      "Bad Request" /*", query parameter shop is mandatory"*/,
+      {
+        status: 400,
+      }
+    );
+  }
+  await handleOrderCancel(orderId, status);
+
+  const response = json("order");
+  response.headers.append("Access-Control-Allow-Origin", "*");
+  return response;
+};
